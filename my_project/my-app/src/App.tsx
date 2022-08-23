0893b7e4a8f593bbcdef18087edef466b8056027
Pok3939 <105784621+Pok3939@users.noreply.github.com>
2022-08-23 22:00:41 +0800

    update layout

--- my_project/my-app/src/App.tsx
+++ my_project/my-app/src/App.tsx
@@ -5,6 +5,25 @@ import GridLayout from "react-grid-layout";
 import '../node_modules/react-grid-layout/css/styles.css'
 import '../node_modules/react-resizable/css/styles.css'
 
+// class MyResponsiveGrid extends React.Component {
+//   render() {
+//     // {lg: layout1, md: layout2, ...}
+//     const layouts = getLayoutsFromSomewhere();
+//     return (
+//       <ResponsiveGridLayout
+//         className="layout"
+//         layouts={layouts}
+//         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+//         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+//       >
+//         <div key="1">1</div>
+//         <div key="2">2</div>
+//         <div key="3">3</div>
+//       </ResponsiveGridLayout>
+//     );
+//   }
+// }
+
 class MyFirstGrid extends React.Component {
   render() {
     // layout is an array of objects, see the demo for more complete usage
@@ -21,32 +40,28 @@ class MyFirstGrid extends React.Component {
         rowHeight={30}
         width={1200}
       >
-        <div className='layout2' key="a">a</div>
-        <div className='layout2' key="b">b</div>
-        <div className='layout2' key="c">c</div>
+        <div className='layout2' key="a"></div>
+        <div className='layout2' key="b"></div>
+        <div className='layout2' key="c"></div>
       </GridLayout>
     );
   }
 }
-
+function Refresh() {
+  alert('Refresh new photos');
+}
 function App() {
   return (
     <div className="App">
-      
+      <text className='Heading'> Eat What?</text>
+      <div className='blackBar'></div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <div className='Photogrid'>
         <MyFirstGrid/>
         <MyFirstGrid/>
+        <MyFirstGrid/>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className='RefreshButton' onClick={Refresh}>🔄</button>
       
       </header>
       
