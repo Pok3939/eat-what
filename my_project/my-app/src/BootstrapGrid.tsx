bb57aebfb744a80b854f30acfbb15c61c94bf518
Pok3939 <105784621+Pok3939@users.noreply.github.com>
2022-08-26 15:36:37 +0800

    grid css update

--- my_project/my-app/src/BootstrapGrid.tsx
+++ my_project/my-app/src/BootstrapGrid.tsx
@@ -7,13 +7,13 @@ function ResponsiveAutoExample() {
   return (
     <Container>
       <Row>
-        <Col><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img></Col>
+        <Col><img src ='./grid1.jpg'></img><img src ='./grid2.jpg'></img><img src ='./grid3.jpg'></img></Col>
       </Row>
       <Row>
-        <Col><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img></Col>
+        <Col><img src ='./grid4.jpg'></img><img src ='./grid5.jpg'></img><img src ='./grid6.jpg'></img></Col>
       </Row>
       <Row>
-        <Col><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img></Col>
+        <Col><img src ='./grid7.jpg'></img><img src ='./grid8.jpg'></img><img src ='./grid9.jpg'></img></Col>
       </Row>
 
       {/* <Row>
--- my_project/my-app/src/RestaurantPage.tsx
+++ my_project/my-app/src/RestaurantPage.tsx
@@ -2,31 +2,34 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 function RestaurantPage() {
-  return (
-    <Container>
-      <Row>
-        <Col sm={8}>
+    return (
+        <Container>
             <Row>
-            <Col sm={12}><img src ='./Food1.jpg'></img></Col>
-            <Col sm = {4}><img src ='./Menu1.jpg'></img></Col>
-            <Col sm = {4}><img src ='./Food2.jpg'></img></Col>
-            <Col sm = {4}><img src ='./Food3.jpg'></img></Col>
+                <Col sm={8}>
+                    <Row>
+                        <Col sm={12}><img src='./Food1.jpg'></img></Col>
+                        <Col sm={4}><img src='./Menu1.jpg'></img></Col>
+                        <Col sm={4}><img src='./Food2.jpg'></img></Col>
+                        <Col sm={4}><img src='./Food3.jpg'></img></Col>
+                    </Row>
+                </Col>
+                <Col sm={4}>
+                    <img src='./restaurant1.jpg'></img>
+                    <div className='RestaurantName'><h3>The Coffee Academics</h3></div>
+                    <div className='PriceRange'><b>Price Range:</b>$50 - $100</div>
+                    <div className='Address'><b>Address:</b>海港城 Shop 4201K, Level 4, Gateway Arcade, 3-27 Canton Rd, Tsim Sha Tsui</div>
+                    <div className='GoogleMap'>GoogleMap</div>
+                    <div className='ChooseThis'><Button variant="primary" size="lg">
+                        就食呢間啦!
+                    </Button></div>
+                </Col>
             </Row>
-        </Col>
-        <Col sm={4}>
-        <img src ='./restaurant1.jpg'></img>
-        <div className='RestaurantName'><h3>The Coffee Academics</h3></div>
-        <div className='PriceRange'><b>Price Range:</b>$50 - $100</div>
-        <div className='Address'>海港城 Shop 4201K, Level 4, Gateway Arcade, 3-27 Canton Rd, Tsim Sha Tsui</div>
-        <div className='GoogleMap'>GoogleMap</div>
 
-        </Col>
-      </Row>
-      
-    </Container>
-  );
+        </Container>
+    );
 }
 
 export default RestaurantPage
