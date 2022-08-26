e5f80d3815cc1bd9f9d7203ff2e3c69dcf2904fd
Pok3939 <105784621+Pok3939@users.noreply.github.com>
2022-08-26 15:01:00 +0800

    Adding outline for grid system

--- my_project/my-app/src/BootstrapGrid.tsx
+++ my_project/my-app/src/BootstrapGrid.tsx
@@ -1,11 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './BootstrapGrid.css'
 
 function ResponsiveAutoExample() {
   return (
     <Container>
       <Row>
+        <Col><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img></Col>
+      </Row>
+      <Row>
+        <Col><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img></Col>
+      </Row>
+      <Row>
+        <Col><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img><img src ='./huge.jpg'></img></Col>
+      </Row>
+
+      {/* <Row>
         <Col sm><img src ='./huge.jpg'></img></Col>
         <Col sm><img src ='./huge.jpg'></img></Col>
         <Col sm><img src ='./huge.jpg'></img></Col>
@@ -19,7 +30,7 @@ function ResponsiveAutoExample() {
         <Col sm><img src ='./huge.jpg'></img></Col>
         <Col sm><img src ='./huge.jpg'></img></Col>
         <Col sm><img src ='./huge.jpg'></img></Col>
-      </Row>
+      </Row> */}
       
     </Container>
   );
